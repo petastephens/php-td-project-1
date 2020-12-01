572495d98de23a3b0c23508be1a64330a681c0be
Peta Stephens <stephenspetaj@gmail.com>
2020-12-01 20:43:41 +0930

    Add exceeds expectation tasks

--- inc/functions.php
+++ inc/functions.php
@@ -5,19 +5,22 @@ $quotes = [
         'quote' => "I feel the need... the need for speed.",
         'source' => "Maverick and Goose",
         'citation' => "Top Gun",
-        'year' => 1986
+        'year' => 1986,
+        'tag' => 'Film & TV'
     ],
     2 => [
         'quote' => "I restore myself when I'm alone. A career is born in public — talent in privacy.",
         'source' => "Marilyn Monroe",
         'citation' => "Ms. magazine",
-        'year' => 1972
+        'year' => 1972,
+        'tag' => 'Film & TV'
     ],
     3 => [
         'quote' => "You're off to great places. Today is your day! Your mountain is waiting. So... get on your way.",
         'source' => "Dr. Seuss",
         'citation' => "Oh, the Places You'll Go!",
-        'year' => 1990
+        'year' => 1990,
+        'tag' => 'Books'
     ],
     4 => [
         'quote' => "He who has a Why? in life can tolerate almost any How?",
@@ -28,11 +31,13 @@ $quotes = [
     5 => [
         'quote' => "In every job that must be done, there is an element of fun. You find the fun and snap! The job's a game.",
         'source' => "Mary Poppins",
-        'year' => 1964
+        'year' => 1964,
+        'tag' => 'Film & TV'
     ],
     6 => [
         'quote' => "Life is what happens while you are busy making other plans.",
         'source' => "John Lennon",
+        'tag' => 'Celebrities'
     ]
 ];
 
@@ -41,6 +46,14 @@ function getRandomQuote($array)
 {
     $randNumber = rand(1, count($array));
     return $array[$randNumber];
+}
+
+// Function to get a random background colour for page body
+function getRandomColour()
+{
+    $colours = ['#FF8C00', '#8FBC8F', '#00CED1', '#BA55D3', '#FA8072', '#008080'];
+    $bgColour = $colours[rand(0, count($colours) - 1)];
+    return $bgColour;
 }
 
 // Function to get a random quote then format for display
@@ -63,7 +76,11 @@ function printQuote($array)
         }
         if ($key === 'year') {
             $this_year = $value;
-            $quoteDisplay .= "<span class=\"citation\">$this_year</span>";
+            $quoteDisplay .= "<span class=\"year\">$this_year</span>";
+        }
+        if ($key === 'tag') {
+            $this_tag = $value;
+            $quoteDisplay .= "<span class=\"tag\">$this_tag</span>";
         }
     }
     $quoteDisplay .= "</p>";
--- index.php
+++ index.php
@@ -5,13 +5,14 @@
 
 <head>
   <meta charset="UTF-8">
+  <meta http-equiv="refresh" content="20">
   <title>Random Quotes</title>
   <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/styles.css">
 </head>
 
-<body>
+<body style="background-color: <?php echo getRandomColour();?>">
   <div class="container">
     <div id="quote-box">
       <?php printQuote($quotes); ?>
